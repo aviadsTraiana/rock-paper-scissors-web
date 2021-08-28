e94834c986b14248e60fcff4ceb7b652a4d4cac9
aviadsTraiana <aviads@traiana.com>
2021-08-28 13:21:28 +0300

    make render func

--- main.go
+++ main.go
@@ -10,7 +10,18 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	// html := `<strong>Hello world</strong>`
 	// w.Header().Set("Content-Type", "text/html")
 	//fmt.Fprintf(w, html)
-	t, err := template.ParseFiles("index.html")
+	renderPage(w, "index.html")
+
+}
+
+func main() {
+	http.HandleFunc("/", homePage)
+	log.Println("Starting web server on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func renderPage(w http.ResponseWriter, page string) {
+	t, err := template.ParseFiles(page)
 	if err != nil {
 		log.Fatal(err)
 		return
@@ -20,11 +31,4 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(err)
 		return
 	}
-
-}
-
-func main() {
-	http.HandleFunc("/", homePage)
-	log.Println("Starting web server on port 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
 }
